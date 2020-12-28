030c15e7bee89f6370da3475f3c3ba2633902a8c
sykim12140226 <sykim12140226@gmail.com>
2020-12-28 22:17:57 +0900

    initial commit

---
+++ opencv_document_scan.py
@@ -0,0 +1,91 @@
+import sys
+import cv2
+import numpy as np
+import os
+
+
+def drawROI(img, corners):
+    cpy = img.copy()
+    c1 = (192, 192, 255)
+    c2 = (128, 128, 255)
+
+    for pt in corners:
+        cv2.circle(cpy, tuple(pt), 25, c1, -1, cv2.LINE_AA)
+
+    cv2.line(cpy, tuple(corners[0]), tuple(corners[1]), c2, 2, cv2.LINE_AA)
+    cv2.line(cpy, tuple(corners[1]), tuple(corners[2]), c2, 2, cv2.LINE_AA)
+    cv2.line(cpy, tuple(corners[2]), tuple(corners[3]), c2, 2, cv2.LINE_AA)
+    cv2.line(cpy, tuple(corners[3]), tuple(corners[0]), c2, 2, cv2.LINE_AA)
+
+    disp = cv2.addWeighted(img, 0.3, cpy, 0.7, 0)
+    return disp
+
+
+def onMouse(event, x, y, flags, param):
+    global srcQuad, dragSrc, ptOld, src
+
+    # 버튼이 눌렸을 때
+    if event == cv2.EVENT_LBUTTONDOWN:
+        for i in range(4):
+            if cv2.norm(srcQuad[i] - (x, y)) < 25:
+                dragSrc[i] = True
+                ptOld = (x, y)
+                break
+
+    if event == cv2.EVENT_LBUTTONUP:
+        for i in range(4):
+            dragSrc[i] = False
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        for i in range(4):
+            if dragSrc[i]:
+                dx = x - ptOld[0]
+                dy = y - ptOld[1]
+
+                srcQuad[i] += (dx, dy)
+
+                cpy = drawROI(src, srcQuad)
+                cv2.imshow('img', cpy)
+                ptOld = (x, y)
+                break
+
+
+src = cv2.imread('keybord.jpg', cv2.IMREAD_COLOR)
+
+if src is None:
+    print('Camera load failed!')
+    sys.exit()
+
+# 입력 영상 크기 및 출력 영상 크기
+h, w = src.shape[:2]
+dw = 500
+dh = round(dw*297/210)
+
+# 모서리 점들의 좌표, 드래그 상태 여부
+srcQuad = np.array(
+    [[30, 30], [30, h - 30], [w - 30, h - 30], [w - 30, 30]], np.float32)
+dstQuad = np.array(
+    [[0, 0], [0, dh - 1], [dw - 1, dh - 1], [dw - 1, 0]], np.float32)
+dragSrc = [False, False, False, False]
+
+# src 초기 화면에 srcQuad 표시하려고 diplay 변수 만듦
+disp = drawROI(src, srcQuad)
+
+cv2.imshow('img', disp)
+cv2.setMouseCallback('img', onMouse)
+
+while True:
+    key = cv2.waitKey()
+    if key == 13:
+        break
+    elif key == 27:
+        cv2.destroyWindow('img')
+        sys.exit()
+
+# 투시변환
+pers = cv2.getPerspectiveTransform(srcQuad, dstQuad)
+dst = cv2.warpPerspective(src, pers, (dw, dh), flags=cv2.INTER_CUBIC)
+
+cv2.imshow('dst', dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
